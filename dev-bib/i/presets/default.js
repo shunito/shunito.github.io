6805ee55139d2f0cffb08fd2d6c9697aeb536707
Shunsuke Ito <tak0tak0>
2017-06-10 16:41:49 +0900

    update GA

--- dev-bib/i/presets/default.js
+++ dev-bib/i/presets/default.js
@@ -64,7 +64,7 @@ Bibi.Preset = {
 
 "extensions": [
     { "name": "Unzipper", "src": "extensions/unzipper/unzipper.js" },
-    { "name": "Analytics", "src" : "extensions/analytics/analytics.js", "tracking-id": "" }, // "tracking-id": Your own Google Analytics tracking id, as "UA-********-*"
+    { "name": "Analytics", "src" : "extensions/analytics/analytics.js", "tracking-id": "UA-58887552-1" }, // "tracking-id": Your own Google Analytics tracking id, as "UA-********-*"
     { "name": "FontSize", "src": "extensions/fontsize/fontsize.js", "base": "auto", "scale-per-step": 1.25 }, // "base": "auto" or pixel-number (if you want to change the default font-size based on the size used most frequently in each HTML)
     { "name": "Share", "src" : "extensions/share/share.js" },
     //{ "name": "EPUBCFI", "src": "extensions/epubcfi/epubcfi.js" },
