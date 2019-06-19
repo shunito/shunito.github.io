80d78a46bcd8cc726e07ce75e2b22bb2cdcfe7e3
Shunsuke Ito <shunsuke.ito@gmail.com>
2019-06-19 20:32:45 +0900

    微振動！

--- fastread/dist/bundle.js
+++ fastread/dist/bundle.js
@@ -57,8 +57,6 @@
             const token = elm[0];
             const type = elm[1];
 
-            console.log( elm );
-
             if (beforeType === 'M-p' || beforeType === 'M-c' || beforeType === 'M-cp'){
                 result.push( chunk );
                 chunk = token;
@@ -93,8 +91,10 @@
         const fontSize = document.getElementById('fontsizeSelect').value;
         const wordsByLine = document.getElementById('wordsByLineSelect').value;
         const baseLine = document.getElementById('baseLineSelect').value;
+        const vibration = document.getElementById('checkVibration').checked;
         const addTopValue = fontSize / 100 * baseLine;
 
+        console.log( 'v ' ,vibration );
         result.classList.remove('hide');
         result.style.height = "";
         result.style.fontSize = `${fontSize}px`;
@@ -110,7 +110,14 @@
         let line, lineWords;
         let topPosition = 0;
         let lines = result.querySelectorAll('p');
+
         for (line of lines) {
+
+            // Add vibration!
+            if( vibration ) {
+                line.classList.add('vibration');
+            }
+        
             lineWords = line.querySelectorAll('span');
             for (let elem of lineWords) {
                 elem.style.top = `${topPosition}px`;
